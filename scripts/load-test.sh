fba01bdee05ff441f263f67469a2217ad5778834
Aaron France <aaron.l.france@gmail.com>
2021-02-16 14:52:36 +0100

    fix: typo in load-test

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -4,17 +4,17 @@
 for loss in $(seq 0 5 25); do
 
 echo "***************************************"
-echo "********* TESIING GO GUIC *************"
+echo "********* TESTING GO GUIC *************"
 echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-quic-test.csv
 
 
 echo "***************************************"
-echo "********* TESIING GO ******************"
+echo "********* TESTING GO ******************"
 echo "GET http://localhost:9002/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-test.csv
 
 
 echo "***************************************"
-echo "********* TESIING NGINX ***************"
+echo "********* TESTING NGINX ***************"
 echo "GET http://localhost:8181/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> nginx-test.csv
 
 done
