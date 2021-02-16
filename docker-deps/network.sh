b2b3a7a7a316194cabb0c0f99e8a4098fcf53903
Aaron France <aaron.l.france@gmail.com>
2021-02-16 02:17:42 +0100

    If the root handle doesn't exist, don't fail

--- docker-deps/network.sh
+++ docker-deps/network.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 IFACE="${IFACE:-eth0}"
 
 echo "Creating horrible network conditions on network interface: ${IFACE}"
 
-tc qdisc del dev "${IFACE}" root
+tc qdisc del dev "${IFACE}" root || true
 tc qdisc add dev "${IFACE}" root handle 1: prio
 tc qdisc add dev "${IFACE}" parent 1:3 handle 30: tbf rate 10000kbps buffer 1600 limit  3000
 tc qdisc add dev "${IFACE}" parent 30:1 handle 31: netem delay 250ms 75ms 25% loss 15% duplicate .05% corrupt 0.1% reorder 2% 50%
