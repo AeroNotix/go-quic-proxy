742da018c72873cf7b047426eb738409c900b6a6
Aaron France <aaron.l.france@gmail.com>
2021-02-16 12:52:37 +0100

    fix: set bin filename correctly for which proxy is being tested

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -5,12 +5,12 @@ for loss in $(seq 0 5 25); do
 
 echo "***************************************"
 echo "********* TESIING GO GUIC *************"
-echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-test.csv
+echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-quic-test.csv
 
 
 echo "***************************************"
 echo "********* TESIING GO ******************"
-echo "GET http://localhost:9002/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-quic-test.csv
+echo "GET http://localhost:9002/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-test.csv
 
 
 echo "***************************************"
