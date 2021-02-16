c25ccb3dcbe0e0cc63ce721e45174053e1e7a8fe
Joe Hughes <jjhughes57@gmail.com>
2021-02-15 22:27:08 -0500

    fix: update load-test.sh

--- load-test.sh
+++ load-test.sh
@@ -1,29 +1,20 @@
 #!/bin/bash 
 
 
-for loss in $(seq 1 3 80); do
+for loss in $(seq 0 5 25); do
 
 echo "***************************************"
 echo "********* TESIING GO  *****************"
-echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv  
-mv loadtest.csv go-test.csv
-sed "s/$/,$loss/" >> go-test.csv
+echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-test.csv  
 
 
 echo "***************************************"
 echo "********* TESIING GO QUIC *************"
-echo "GET http://localhost:9002/debug" | vegeta attack -duration=10s --rate=4  | tee results.bin | vegeta report
+echo "GET http://localhost:9002/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-quic-test.csv
 
-
-mv loadtest.csv go-quic-test.csv
-sed "s/$/,$loss/" >> go-quic-test.csv
 
 echo "***************************************"
 echo "********* TESIING NGINX ***************"
-echo "GET http://localhost:8181/debug" | vegeta attack -duration=10s --rate=4  | tee results.bin | vegeta report
-
-
-mv loadtest.csv nginx-test.csv
-sed "s/$/,$loss/" >> nginx-test.csv
+echo "GET http://localhost:8181/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> nginx-test.csv 
 
 done
