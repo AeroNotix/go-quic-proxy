d0223de82488681cfbe08a5de2193270d60c5f9f
Aaron France <aaron.l.france@gmail.com>
2021-02-16 12:30:02 +0100

    fix: set header correctly for which proxy is being tested

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,20 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 
 
 for loss in $(seq 0 5 25); do
 
 echo "***************************************"
-echo "********* TESIING GO  *****************"
-echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-test.csv  
+echo "********* TESIING GO GUIC *************"
+echo "GET http://localhost:9001/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-test.csv
 
 
 echo "***************************************"
-echo "********* TESIING GO QUIC *************"
+echo "********* TESIING GO ******************"
 echo "GET http://localhost:9002/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> go-quic-test.csv
 
 
 echo "***************************************"
 echo "********* TESIING NGINX ***************"
-echo "GET http://localhost:8181/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> nginx-test.csv 
+echo "GET http://localhost:8181/debug" | vegeta attack -duration=10s --rate=4  | vegeta dump -dumper csv | sed "s/$/,$loss/" >> nginx-test.csv
 
 done
